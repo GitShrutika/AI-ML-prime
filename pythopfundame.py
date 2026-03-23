ff2c286d6644f9d0f770a8f38494c2f201a64bb3
Shrutika <shrutika.gitdesk@gmail.com>
2026-03-23 23:25:33 +0530

    Create pythonfundame.py

---
+++ pythopfundame.py
@@ -0,0 +1,38 @@
+print("hello world")
+name="shrutika"
+age = 19
+PI = 3.14
+print("my name is:",name)
+print("my age is: ",age+1)
+print(type(PI))
+num = 2
+isPrime = True
+print(type(isPrime))
+#commit
+a=2
+b=3
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a**b)
+print(a%b)
+print(a==b)
+print(a!=b)
+print(a>=b)
+c=5
+c+=4
+print(c)
+var=False
+print(not var)
+print((5>3)and(5<6))
+d=input("enter val for a")
+print(d)
+e=int(input("enter a:"))
+f=int(input("enter b:"))
+sum=e+f
+print(sum)
+g=int(input("enter g:"))
+h=int(input("enter h:"))
+avg=(g+h)/2
+print(avg)
