d5f6b78508656d932d88ce5252249d35aaef35ae
Shrutika <shrutika.gitdesk@gmail.com>
2026-03-27 23:30:30 +0530

    Create conditional.py

---
+++ conditional.py
@@ -0,0 +1,28 @@
+age = int(input("ener your age"))
+if age >= 18:
+    print("eligible for vott")
+else:
+    print(" not eligible for vote")
+color = input("enter a color:")
+    
+if color== "red":
+     print("stop")
+elif color == "yello":
+     print("go slow")
+elif color=="yellow":
+     print("stop")
+
+age = int(input("enter your age:"))
+
+if (age<13):
+          print("child")
+elif(age>=13 and age<18):
+          print("teen")
+userame =input("enter your user name")
+password =input("enter your password")
+if (userame=="admin" and password=="pass"):
+    print("login success")
+elif(userame != "admin"):
+    print("wrong user")
+else:
+    print("wrong pass")
