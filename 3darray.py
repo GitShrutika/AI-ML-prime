18d429790c295301f12d3ecb7f26148a54ff6599
Shrutika <shrutika.gitdesk@gmail.com>
2026-04-12 00:42:27 +0530

    Create 3darray.py

---
+++ 3darray.py
@@ -0,0 +1,10 @@
+arr=np.array([1,2,3,4,5])
+arr2=([[1,2,3,4,5],[1,2,3,4,5]])
+print(arr+arr2)
+import numpy as np
+arr=([[6,8],[4,5]])
+mean = print(np.mean(arr1))
+stdr = print(np.std(arr1))
+print(arr1)
+normali_arr = (arr-mean)/stdr
+print(normali_arr)
